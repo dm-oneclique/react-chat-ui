d8c46828f60dad19459eb39c781a1b8be0ad054e
dm-oneclique <dm@oneclique.com>
2025-11-17 16:25:21 -0300

    fix: export more types

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,48 @@
+import ConversationHeader from "./components/conversation-header";
+import ConversationList from "./components/conversation-list";
+import Loading from "./components/loading";
+import MainContainer from "./components/main-container";
+import Message from "./components/message";
+import MessageContainer from "./components/message-container";
+import MessageHeader from "./components/message-header";
+import MessageInput from "./components/message-input";
 import MessageList from "./components/message-list";
 import MessageListBackground from "./components/message-list-background";
-import ConversationList from "./components/conversation-list";
-import MainContainer from "./components/main-container";
-import Loading from "./components/loading";
-import Message from "./components/message"
-import MessageInput from "./components/message-input";
-import MessageHeader from "./components/message-header";
-import MessageContainer from "./components/message-container";
-import ConversationHeader from "./components/conversation-header";
 import TypingIndicator from "./components/typing-indicator";
 import useCheckIsMobile from "./hooks/useCheckIsMobile";
 import useTypingListener from "./hooks/useTypingListener";
 
-import Sidebar from "./components/sidebar"
+import Conversation from "./components/conversation";
+import Sidebar from "./components/sidebar";
 import MinChatUiProvider from "./providers/MinChatUiProvider";
 import type { ConversationType } from "./types/ConversationType";
+import type { ActionDescription, AttachedFileType, MediaType, MessageType } from "./types/MessageType";
 import type { UserType } from "./types/UserType";
-import type { MessageType } from "./types/MessageType";
-import Conversation from "./components/conversation";
 
 
 
 
 export {
-  Loading,
-  Message,
-  MessageInput,
-  MessageHeader,
-  MessageList,
-  ConversationList,
   Conversation,
-  MainContainer,
   ConversationHeader,
+  ConversationList,
+  Loading,
+  MainContainer,
+  Message,
+  MessageContainer,
+  MessageHeader,
+  MessageInput,
+  MessageList,
+  MessageListBackground,
+  MinChatUiProvider,
+  Sidebar,
   TypingIndicator,
   useCheckIsMobile,
   useTypingListener,
-  Sidebar,
-  MessageListBackground,
-  MessageContainer,
-  MinChatUiProvider,
-
+  type ActionDescription,
+  type AttachedFileType,
   type ConversationType,
-  type UserType,
-  type MessageType
-}
+  type MediaType,
+  type MessageType,
+  type UserType
+};
